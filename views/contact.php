a944fed0e1be16192288e2180901ec6a17609fc6
Kimngan050318 <kimngansv0503@gmail.com>
2024-07-31 21:21:27 +0700

    Add files via upload

---
+++ views/contact.php
@@ -0,0 +1,62 @@
+<!-- Breadcrumb Begin -->
+<div class="breadcrumb-option">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb__links">
+                        <a href="./index.html"><i class="fa fa-home"></i> Home</a>
+                        <span>Contact</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb End -->
+
+    <!-- Contact Section Begin -->
+    <section class="contact spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <div class="contact__content">
+                        <div class="contact__address">
+                            <h5>THÔNG TIN LIÊN LẠC</h5>
+                            <ul>
+                                <li>
+                                    <h6><i class="fa fa-map-marker"></i> Địa chỉ</h6>
+                                    <p>699 Trương Định , Thịnh Liệt , Hoàng Mai , Hà Nội</p>
+                                </li>
+                                <li>
+                                    <h6><i class="fa fa-phone"></i> Số điện thoại</h6>
+                                    <p><span>0368072396</span><span>0396200776</span></p>
+                                </li>
+                                <li>
+                                    <h6><i class="fa fa-headphones"></i> Hỗ trợ</h6>
+                                    <p>kimngansv0503@gmail.com</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="contact__form">
+                            <h5>
+                                GỬI TIN NHẮN</h5>
+                            <form action="#">
+                                <input type="text" placeholder="Họ tên">
+                                <input type="text" placeholder="Email">
+                                <input type="text" placeholder="Website">
+                                <textarea placeholder="Tin nhắn"></textarea>
+                                <button type="submit" class="site-btn">GỬI TIN NHẮN</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="contact__map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.3690725602714!2d105.83837917971427!3d20.97783773078879!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac459555a6b3%3A0x11977ace6c727344!2zTmcuIDY5OSBUcsawxqFuZyDEkOG7i25oLCBHacOhcCBOaOG7iywgSG_DoG5nIE1haSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1720584124241!5m2!1svi!2s" width="600" height="780" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        
+                    </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact Section End -->
+
